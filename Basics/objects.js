4e9642e57365d83eb18744966bc3763fcc4e8a30
Shashank Singh <singhshashank875@gmail.com>
2024-09-27 11:33:30 +0530

    object updated

--- Basics/objects.js
+++ Basics/objects.js
@@ -44,3 +44,24 @@ console.log(myobj['channel'])
 console.log(myobj.channel)
 console.log(myobj.marks)
 console.log(myobj.isActive)
+
+jsonobj = {
+    names: ['Shasha', 'shiv', 'san', 'shub'],
+    foods: ['momos', 'rolls', 'shake', 'drinks'],
+    home: 'kanpur',
+    drink: 'MC float',
+    deal: 'Meal',
+}
+// console.log(jsonobj)
+
+// convert to str 
+let strobj = JSON.stringify(jsonobj)
+console.log(strobj)
+
+// replace in str
+strobj = strobj.replace('kanpur', 'India')
+console.log(strobj)
+
+// back to json obj 
+newobj = JSON.parse(strobj)
+console.log(newobj)
