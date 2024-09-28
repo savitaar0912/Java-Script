84adf577d5bccdd9711a1632a55934c8c86ed88e
Shashank Singh <singhshashank875@gmail.com>
2024-09-28 18:04:41 +0530

    table in code7

--- Basics/sumofIndex.js
+++ Basics/sumofIndex.js
@@ -4,12 +4,14 @@ let arr = [3,3]
 let goal = 6
 // Output = 1,2
 
-for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-        if((arr[i]+arr[j] === goal)&&i!=j){
-            console.log([i,j])
-            console.log([arr[i],arr[j]])
-            // break;
+for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === goal) {
+            console.log([i, j]);
+            console.log([arr[i], arr[j]]);
+            return; // Exit the function after finding the first pair
         }
     }
 }
+
+console.log("No pair found");
